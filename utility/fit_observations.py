c994a07ca817457e2dfce59a79aa47259a6370b9
Ekaterina Semenova <semenova@mpia.de>
2022-10-26 16:32:26 +0200

    trying a lot of things to improve the fit

--- utility/fit_observations.py
+++ utility/fit_observations.py
@@ -7,6 +7,7 @@ from sys import argv, exit
 import glob
 import pickle
 from scipy import interpolate
+from scipy.optimize import least_squares, minimize
 import sys
 sys.path.append('/home/semenova/codes/ts-wrapper/TurboSpectrum-Wrapper/utility/')
 from observations import readSpectrumTSwrapper, spectrum, read_observations, convolve_gauss
@@ -16,50 +17,65 @@ from PayneModule import restore, restoreFromNormLabels, readNN
 import cProfile
 import pstats
 import time
-import shutil
+import shutil, os
 from IPython.display import clear_output
 
 def normalisePayneLabels(labels, xmax, xmin):
     return (labels-xmin)/(xmax-xmin) - 0.5
+def denormalisePayneLabel(labels, xmax, xmin):
+    return (labels + 0.5) * (xmax - xmin) + xmin
 
-def callNN(wavelength, obsSpec, NNdict, p0, freeLabels, setLabels, mask, quite=True):
+def callNN(p0, *args):
+    wavelength, obsSpec, NNdict, freeLabels, setLabels, norm, mask, quite = args
     """
      To ensure the best convergence this function needs to be called on normalised labels (in p0)
      maybe it would work withput normalisation? it would make the code so much nicer
     """
   #  setLabels[i] = (setLabels[i] - norm['min'][i] ) / ( norm['max'][i] - norm['min'][i] ) - 0.5
 
+    setLabels = normalisePayneLabels(setLabels, norm['max'], norm['min'])
     labels = setLabels.copy()
     labels[freeLabels] = p0
-    print(labels[freeLabels])
-    Vbroad = labels[-3]
-    rv = labels[-2]
-    offset = labels[-1]
+    fitLab = denormalisePayneLabel(labels[freeLabels], norm['max'][freeLabels], norm['min'][freeLabels])
+    offset = denormalisePayneLabel( labels[-3], norm['max'][-3], norm['min'][-3] )
+    Vbroad = denormalisePayneLabel( labels[-2], norm['max'][-2], norm['min'][-2] )
+    rv = denormalisePayneLabel( labels[-1], norm['max'][-1], norm['min'][-1] )
+    
     flux, wvl = [], []
     for NNid, ANN in NNdict.items():
-        f = restore(ANN['wvl'], ANN, labels[:-3])
-        flux = np.hstack( (flux,  f) )
-        wvl = np.hstack( (wvl,  ANN['wvl']) )
+        if min(wavelength) > min(ANN['wvl']) and max(ANN['wvl']) > max(wavelength):
+            f = restoreFromNormLabels(ANN['wvl'], ANN, labels[:-3])
+            flux = np.hstack( (flux,  f) )
+            wvl = np.hstack( (wvl,  ANN['wvl']) )
     wvl += rv
-    if Vbroad > 0.0:
-        flux = convolve_gauss(wavelength, flux, Vbroad, mode='broad')
-    if obsSpec.R < ANN['res']:
-        flux = convolve_gauss(wavelength, flux, obsSpec.R, mode='res')
     flux = np.interp(wavelength, wvl, flux)
+    spec = spectrum(wavelength, flux)
+
+    if obsSpec.R < ANN['res']:
+        spec.convolve_resolution(obsSpec.R)
+        #stdR, flux = convolve_gauss(wavelength, flux, obsSpec.R, mode='res')
+    if Vbroad > 0.0:
+        spec.convolve_macroturbulence(Vbroad)
+        #stdV, flux = convolve_gauss(wavelength, flux, Vbroad, mode='broad')
+    flux = spec.flux
     flux += offset
 
-    chi2 = np.sqrt(np.sum(obsSpec.flux[mask] - flux)**2)
-    fits = glob.glob('./bestFit_*.txt')
-    if len(fits) > 0:
-        ind = [ s.split('/')[-1].split('_')[-1].replace('.txt', '') for s in fits ]
-        i = np.max( np.array(ind).astype(int)) + 1
-    else:
-        i = 0
-    np.savetxt(f'./bestFit_{i:.0f}.txt', np.vstack([wavelength, obsSpec.flux[mask], flux]).T )
-    print(f"chi^2 = {chi2:.2f}")
-    return flux
+    chi2 = np.sum( (obsSpec.flux[mask] - flux)**2 / flux )
+    
+    #print(fitLab)
+    #fits = glob.glob('./bestFit_*.txt')
+    #if len(fits) > 0:
+    #    ind = [ s.split('/')[-1].split('_')[-1].replace('.txt', '') for s in fits ]
+    #    i = np.max( np.array(ind).astype(int)) + 1
+    #else:
+    #    i = 0
+    #np.savetxt(f'./bestFit_{i:.0f}.txt', np.vstack([wavelength, obsSpec.flux[mask], flux]).T )
 
-def fitToNeuralNetwork(obsSpec, NNdict, prior = None, quite = True):
+    #correlation = np.correlate(1-flux, 1-specObs.flux[mask])[0]
+    #print(f"chi^2 = ", [f'{chi2:.4f}' if chi2 > 1 else f'{chi2:.4e}'][0])
+    return chi2
+
+def fitToNeuralNetwork(obsSpec, NNdict, prior = None, quite = True, initLabels = None):
     for NNid, NN0 in NNdict.items():
         break
     freeLabels = np.full(len(NN0['labelsKeys'])+3, True)
@@ -67,8 +83,8 @@ def fitToNeuralNetwork(obsSpec, NNdict, prior = None, quite = True):
     if isinstance(prior, type(None)):
         pass
     else:
-        if len(prior)  < len(NN0['labelsKeys']) + 1:
-            for i, l in enumerate( np.hstack( (NN0['labelsKeys'], ['vbroad', 'rv', 'offset']))):
+        if len(prior)  < len(NN0['labelsKeys']) + 3:
+            for i, l in enumerate( np.hstack( (NN0['labelsKeys'], ['offset', 'vbroad', 'rv']))):
                 if l in prior or l.lower in prior:
                     freeLabels[i] = False
                     try:
@@ -85,57 +101,43 @@ def fitToNeuralNetwork(obsSpec, NNdict, prior = None, quite = True):
     Extra dimension is for macro-turbulence and rotation
     """
 
-    initLabels = []
-    norm = {'min' : np.hstack( [NN0['x_min'], [1, -0.3, -0.1]] ), 'max': np.hstack( [NN0['x_max'], [100, 0.3, 0.1]] ) }
-    for i, l in enumerate( np.hstack( (NN0['labelsKeys'], ['vbroad', 'rv', 'offset']))):
-        if freeLabels[i]:
-            initLabels.append( np.mean( (norm['min'][i], norm['max'][i] ) ) )
-    #"""
-    #Resampled (and cut if needed)  observed spectrum to the wavelength points
-    #provided in the ANN
-    #"""
-    #w_new = []
-    #for ann in NNdict.values():
-    #    w_new = np.hstack( (w_new, ann['wvl']) )
-    #w_new = w_new[np.logical_and( w_new>min(obsSpec.lam ), w_new<max(obsSpec.lam) )]
-    #obsSpec.flux = np.interp(w_new,  obsSpec.lam, obsSpec.flux)
-    #obsSpec.lam = w_new
+    norm = {'min' : np.hstack( [NN0['x_min'], [-0.1, 1, -1]] ), 'max': np.hstack( [NN0['x_max'], [0.1, 50, 1]] ) }
+    if isinstance(initLabels, type(None)):
+        initLabels = np.zeros( len(setLabels) )
 
-    #y = np.gradient(specObs.flux)
-    #chi2mask = np.where(np.abs(y) > np.std(y))
-    #chi2mask = np.where(1.-specObs.flux > 3*1/np.mean(specObs.SNR)  )
-    chi2mask = len(specObs.flux) * [True]
-    if len(specObs.flux[chi2mask]) > 0.7 * len(specObs.flux) :
+    chi2mask = np.full(len(obsSpec.flux), True)
 
 
-        """
-        Lambda function for fitting
-        """
-        fitFunc = lambda wavelength, *labels : callNN(
-                                                    wavelength, obsSpec,
-                                                    NNdict, labels, freeLabels, setLabels, chi2mask, quite = quite
-                                                    )
-        bounds = ( norm['min'][freeLabels], norm['max'][freeLabels] )
-        print(f"Bounds: {bounds}")
-        try:
-            popt,_ = curve_fit(
-                            fitFunc, obsSpec.lam[chi2mask], \
-                            obsSpec.flux[chi2mask], p0=initLabels,\
-                            bounds = bounds,
-                            method = 'dogbox',
+    """
+    Lambda function for fitting
+    """
+    fitFunc = lambda wavelength, *labels : callNN( labels, *(wavelength, obsSpec,
+                                                NNdict,  freeLabels, setLabels, norm, chi2mask, quite)
+                                                )
+    #bounds = ( norm['min'][freeLabels], norm['max'][freeLabels] )
+    bounds = []
+    for i in range(np.sum(freeLabels)):
+        bounds.append( [-0.5, 0.5] )
+    try:
+        res = minimize(
+                             callNN, initLabels[freeLabels], args=(obsSpec.lam[chi2mask], obsSpec,
+                                          NNdict,  freeLabels, setLabels, norm, chi2mask, quite),
+                                       bounds = bounds, 
+                                       options = {'disp' : True, 'maxiter' : int(1e4) },
+                                       method = 'Nelder-Mead', 
+                                       #method = 'Powell' 
                             )
-        except RuntimeError:
-            return np.full(len(setLabels), np.nan), np.nan
-        " restore normalised labels "
-        setLabels[freeLabels] = popt
-
-        wavelength = obsSpec.lam
-        #chi2 = np.sqrt(np.sum(obsSpec.flux - flux)**2)
-        chi2 = np.inf
-        return setLabels, chi2
-    else:
+    except RuntimeError:
         return np.full(len(setLabels), np.nan), np.nan
-
+    " restore normalised labels "
+    setLabels[freeLabels] = res.x
+    setLabels[freeLabels] = denormalisePayneLabel(setLabels[freeLabels], norm['max'][freeLabels], norm['min'][freeLabels])
+ 
+    chi2 = res.fun
+    #if chi2 > 1e-2:
+    #     return np.full(len(setLabels), np.nan), np.nan
+   
+    return setLabels, chi2
 
 def internalAccuracyFitting(nnPath, specList, solveFor=None, lam_limits = [-np.inf, np.inf]):
     print(f"Solving for {solveFor}...")
@@ -189,7 +191,9 @@ def internalAccuracyFitting(nnPath, specList, solveFor=None, lam_limits = [-np.i
                 prior['vbroad'] = 0.0
                 prior['rv'] = 0.0
 
-            labelsFit, bestFitChi2 = fitToNeuralNetwork(obsSpec, NN, prior = prior, quite=True)
+            labelsFit, _ = fitToNeuralNetwork(obsSpec, NN, prior = prior, quite=True, initLabels=None)
+            print('repeating')
+            labelsFit, bestFitChi2 = fitToNeuralNetwork(obsSpec, NN, prior = prior, quite=True, initLabels=labelsFit)
             for i, l in enumerate(NN0['labelsKeys']):
                 if l not in out:
                     out.update({l:[]})
@@ -333,23 +337,8 @@ def MCMCwithANN(NNpath, spec, nwalkers=32, steps=100, startingGrid = None):
 
 
 
-if __name__ == '__main__':
-    if len(argv) < 3:
-        print("Usage: $ python ./fit_observations.py \
-<path to model spectra or payne NN> <path to observed spectra> ")
-        exit()
-    # profiler = cProfile.Profile()
-    # profiler.enable()
-
-    "Fit using Payne neural network"
-    nnPath = argv[1]
-    NNs = glob.glob(nnPath)
-    print(f"found {len(NNs)} ANNs")
-    obsPath = argv[2]
-    specList = glob.glob(obsPath)
-    #specList = glob.glob('./uves_test/ksi_Hya/uvu_11325994-3151279_520.0_25_reNormCont.asc')
-    print(f"found {len(specList)} observed spectra")
-
+def fit_uves_spectra_withPrior(arg):
+    nnPath, specList, ind, linemaskFile=arg
     Jofre = {
     'HD122563' : {'teff':4587, 'logg':1.61, 'vturb':1.92, 'feh':-2.64,
                   'Mg':5.296, 'Mn':2.196, 'Co':2.248, 'Ni':3.493, 'vsini':5.0, 'key':'14023168+0941090'},
@@ -362,48 +351,108 @@ if __name__ == '__main__':
     'mu_Leo' : {'teff':4474, 'logg':2.51, 'vturb':1.28 , 'feh': 0.25,
                   'Mg':8.116, 'Mn': 5.387, 'Co':5.342, 'Ni':6.504, 'vsini':5.1,  'key':'09524561+2600243'},
     }
+    NNs = glob.glob(nnPath)
+    print(f"found {len(NNs)} ANNs")
 
-    lim = None
-    lim = [6176, 6178]
+    if not isinstance(linemaskFile, type(None)):
+        print(f"using linemask from {linemaskFile}")
+        
+        lims = np.loadtxt(linemaskFile, comments=';', usecols=(0,1,2), ndmin = 1)
+    else:
+        lims = [None]
 
     ANNs = {}
     for nnPath in NNs:
            NN = readNN(nnPath, quite=True)
            ANNs[nnPath.split('/')[-1]] = NN
-    with open('./fittingResults.dat', 'w') as fOut:
-            fOut.write('#  ' + '   '.join(f"{k}" for k in NN['labelsKeys']) + ' Vbroad RV  offset chi  SNR\n' )
+    fOut = open(f'./fittingResults_{ind}.dat', 'w')
+    fOut.write('#  ' + '   '.join(f"{k}" for k in NN['labelsKeys']) + '  offset Vbroad RV chi  SNR line \n' )
+    for sp in specList:
+        starID = sp.split('/')[-2]
+        print(starID, sp)
+        w, f, snr = np.loadtxt(sp, unpack=True, usecols=(0,1,2))
+        snr = snr[0]
+        spec = spectrum(w, f, res=47000)
+        spec.ID = sp.split('/')[-1].replace('.asc', '')
+        spec.SNR = snr
 
-            #profiler = cProfile.Profile()
-            #profiler.enable()
-            for sp in specList:
-                starID = sp.split('/')[-2]
-                print(starID)
-                w, f, snr = np.loadtxt(sp, unpack=True, usecols=(0,1,2))
-                snr = snr[0]
-                if isinstance(lim, type(None)):
-                    lim = [ min(w), max(w) ]
-                if  lim[0] >= min(w) and  lim[1] <= max(w):
-                    print(sp)
-                    specObs = spectrum(w, f, res=47000)
-                    specObs.ID = sp.split('/')[-1].replace('.asc', '')
-                    specObs.SNR = snr
-                    specObs.cut(lim)
-                    print(len(specObs.lam))
-                    #prior = None
-                    prior = { k: Jofre[starID][k] for k in ['teff', 'logg', 'feh', 'vturb', 'Mg', 'Mn', 'Co']}
-                    prior['offset'] = 0.0
-                    print(prior)
-                    labelsFit, bestFitChi2 = fitToNeuralNetwork(specObs, ANNs, prior = prior, quite=True)
-                    for i, k in  enumerate(NN['labelsKeys']):
-                        print(f"{k} = {labelsFit[i]:.2f}")
-                    print(f"Vbroad = {labelsFit[-3]:.2f}")
-                    print(f"RV = {labelsFit[-2]:.2f}")
-                    print(f"offset = {labelsFit[-1]:.2f}")
-                    fOut.write(f"{specObs.ID}  " +  '   '.join( f"{l:.3f}" for l in labelsFit  ) + f"  {bestFitChi2:.3f}   {snr:.1f}"  + '\n')
-            #profiler.disable()
-            #stats = pstats.Stats(profiler).sort_stats('cumulative')
-            #stats.print_stats()
-#profiler.disable()
-#with open('./log_profiler.txt', 'w') as stream:
-#    stats = pstats.Stats(profiler, stream = stream).sort_stats('cumulative')
-#    stats.print_stats()
+        for lim in lims:
+            if isinstance(lim, type(None)):
+                lim = [ np.nan, -np.inf, np.inf ]
+            print(f"fitting line at {lim[0]:.2f} AA ")
+            
+            specObs = spec.copy()
+            specObs.cut( [lim[1], lim[2]] )
+            specObs.ID +=  f"{lim[0]:.2f}_AA"
+             
+            if len(specObs.lam) > 0:
+                #prior = None
+                prior = { k: Jofre[starID][k] for k in ['teff', 'logg', 'feh', 'vturb', 'Mg', 'Mn', 'Co']}
+                prior['offset'] = 0.0
+                labelsFit, bestFitChi2 = fitToNeuralNetwork(specObs, ANNs, prior = prior, quite=True)
+                for i, k in  enumerate(NN['labelsKeys']):
+                    print(f"{k} = {labelsFit[i]:.2f}")
+                print(f"offset = {labelsFit[-3]:.2f}")
+                print(f"Vbroad = {labelsFit[-2]:.2f}")
+                print(f"RV = {labelsFit[-1]:.2f}")
+                fOut.write(f"{specObs.ID}  " +  '   '.join( f"{l:.3f}" for l in labelsFit  ) + f"  {bestFitChi2:.5e}   {snr:.0f}  {lim[0]}"  + '\n')
+
+                # save best fit flux
+                specObs = spec.copy()
+                specObs.ID = spec.ID
+                specObs.cut( [lim[1]-2, lim[2]+2] )
+                flux, wvl = [], []
+                for NNid, ANN in ANNs.items():
+                    if min(specObs.lam) > min(ANN['wvl']) and max(ANN['wvl']) > max(specObs.lam):
+                        f = restore(ANN['wvl'], ANN, labelsFit[:-3])
+                        flux = np.hstack( (flux,  f) )
+                        wvl = np.hstack( (wvl,  ANN['wvl']) )
+                wvl += labelsFit[-1]
+                flux = np.interp(specObs.lam, wvl, flux)
+                specMod = spectrum(specObs.lam, flux)
+            
+                if specObs.R < ANN['res']:
+                    specMod.convolve_resolution(specObs.R)
+                Vbroad = labelsFit[-2]
+                if Vbroad > 0.0:
+                    specMod.convolve_macroturbulence(Vbroad)
+                    #stdV, flux = convolve_gauss(wavelength, flux, Vbroad, mode='broad')
+                flux = specMod.flux + labelsFit[-3]
+                del specMod
+                np.savetxt(f'./bestFit_{specObs.ID.strip()}_line{lim[0]:.2f}_SNR{specObs.SNR:.0f}.txt', np.vstack([specObs.lam, specObs.flux, flux]).T )
+    fOut.close()
+    return f'./fittingResults_{ind}.dat'
+
+if __name__ == '__main__':
+    if len(argv) < 3:
+        print("Usage: $ python ./fit_observations.py \
+<path to model spectra or payne NN> <path to observed spectra> ")
+        exit()
+    # profiler = cProfile.Profile()
+    # profiler.enable()
+
+    "Fit using Payne neural network"
+    nnPath = argv[1]
+    obsPath = argv[2]
+    specList = glob.glob(obsPath)
+    print(f"found {len(specList)} observed spectra")
+    if len(argv) > 2:
+        linemaskFile = argv[3]
+    else: 
+        linemaskFile = None
+    if len(argv) > 3:
+        ncpu = int(argv[4])
+    else: 
+        ncpu = 1
+
+    ind = np.arange(len(specList))
+    args = [ [nnPath, specList[i::ncpu], i, linemaskFile ] for i in range(ncpu) ]
+    with Pool(processes=ncpu) as pool:
+        out = pool.map(fit_uves_spectra_withPrior, args )
+    with open('./fittingResults.dat', 'w') as fOut:
+        for f in out:
+            for l in open(f, 'r').readlines():
+                fOut.write( l )
+            os.remove(f)
+    
+
